5369e6fc5d16350ba22051abf202b36d7e4ac80c
robin <robin@ansvia.com>
2014-04-19 15:09:12 +0700

    upgrade ansvia-commons to 0.1.7

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     "typesafe repo"   at "http://repo.typesafe.com/typesafe/releases/",
     "glassfish repo"  at "http://download.java.net/maven/glassfish/",
     "spray repo"      at "http://repo.spray.cc/",
-    "Ansvia repo" at "http://scala.repo.ansvia.com/releases/"
+    "Ansvia repo"     at "http://scala.repo.ansvia.com/releases/"
   )
 
   def compile   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "compile")
@@ -14,7 +14,7 @@ object Dependencies {
   def runtime   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "runtime")
   def container (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
-  val ansviaCommons = "com.ansvia" % "ansvia-commons" % "0.0.6"
+  val ansviaCommons = "com.ansvia" % "ansvia-commons" % "0.1.7"
   val specs2        = "org.specs2" %%  "specs2" % "1.12.4"
   val logback       = "ch.qos.logback" % "logback-classic" % "1.0.9"
 }
