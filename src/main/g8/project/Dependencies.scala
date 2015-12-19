9fc0670ee574cb3b5b89dd27b750d269295bbe90
Robin Syihab <obin.mf@gmail.com>
2015-12-19 15:56:21 +0700

    Update Dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -1,10 +1,9 @@
 import sbt._
 
 object Dependencies {
+
   val resolutionRepos = Seq(
     "typesafe repo"   at "http://repo.typesafe.com/typesafe/releases/",
-    "glassfish repo"  at "http://download.java.net/maven/glassfish/",
-    "spray repo"      at "http://repo.spray.cc/",
     "Ansvia repo"     at "http://scala.repo.ansvia.com/releases/",
     "Ansvia snapshot repo" at "http://scala.repo.ansvia.com/nexus/content/repositories/snapshots",
     "Sonatype snapshots"     at "https://oss.sonatype.org/content/repositories/snapshots",
@@ -17,8 +16,7 @@ object Dependencies {
   def runtime   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "runtime")
   def container (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
-  val ansviaCommons = "com.ansvia" % "ansvia-commons" % "0.1.8-20140421-SNAPSHOT"
+  val ansviaCommons = "com.ansvia" %% "ansvia-commons" % "0.2.2"
   val specs2        = "org.specs2" %% "specs2" % "2.4"
   val logback       = "ch.qos.logback" % "logback-classic" % "1.0.9"
-  //lazy val slick         = "com.typesafe.slick" %% "slick" % "2.1.0-M2"
 }
